ea63f021dfb46d3293989df507dc2fcedfb66ea3
dydel <dan.chodnicki@gmail.com>
2016-06-27 12:28:36 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>CSS Justify</title>
+ </head>
+ <body>
+  <h1>CSS text-align: justify</h1>
+  <p>This page was created to explore possibilities of CSS's text-align: justify atribute.</p>
+ </body>
+</html>
